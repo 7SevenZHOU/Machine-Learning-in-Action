3e1b41b9b8350a2a1e334cbd38f3f18fe6371ca0
Seven ZHOU <zhou@Sevens-MacBook-Pro.local>
2017-10-25 21:36:35 +0800

    naive bayes

--- Ch04/bayes.py
+++ Ch04/bayes.py
@@ -17,15 +17,65 @@ def createVocabList(dataSet):
     for document in dataSet:
         # | operator means set union
         vocabSet=vocabSet|set(document)
-    reutrn list(vocabSet)
+    return list(vocabSet)
 
 def setOfWords2Vec(vocabList,inputSet):
     returnVec=[0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)]=1
-        else:print('the word:%s is not in my Vocabulary!'%word)
+        else:
+            print('the word:%s is not in my Vocabulary!'%word)
     return returnVec
+
+listOPosts,listClasses=loadDataSet()
+myVocabList=createVocabList(listOPosts)
+print(myVocabList)
+
+#testList=['my','cat','dog']
+#setOfWords2Vec(myVocabList,testList)
+#print(setOfWords2Vec(myVocabList,listOposts[2]))
+
+import numpy as np
+
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs=len(trainMatrix)
+    numWords=len(trainMatrix[0])
+    pAbusive=sum(trainCategory)/float(numTrainDocs)
+    #zeros() method is not a native python method
+    p0Num=np.zeros(numWords)
+    p1Num=np.zeros(numWords)
+    p0Denom=0.0
+    p1Denom=0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+        else:
+            p0Num+=trainMatrix[i]
+            p0Denom+=sum(trainMatrix[i])
+    p1Vect=p1Num/p1Denom
+    p0Vect=p0Num/p0Denom
+    return p0Vect,p1Vect,pAbusive
+
+trainMat=[]
+for postinDoc in listOPosts:
+    trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+
+p0V,p1V,pAb=trainNB0(trainMat,listClasses)
+print(pAb)
+print(p0V)
+print(p1V)
+
+
+
+
+
+
+
+
+
+
 
 
 
